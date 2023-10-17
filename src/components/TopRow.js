f43868a76d248d0e56361596402c88a6174f8c13
Poison159 <siyabongahdb@gmail.com>
2023-10-17 13:55:52 +0200

    made cards dynamic

--- src/components/TopRow.js
+++ src/components/TopRow.js
@@ -5,51 +5,55 @@ import CustomAccordion from './CustomAccordion';
 
 export default function Toprow() {
 
-  var appItems = [
-    {imgPath: "https://img.stackshare.io/service/11331/asp.net-core.png", title:".Net C#", description:"We use C# for our backend"},
-    {imgPath: "https://th.bing.com/th/id/OIP.HLxLvGJV-6r8ZR8AK4F9KAHaEO?pid=ImgDet&rs=1", title:"React", description:"We use react for our frontend"},
-    {imgPath: "https://cdn-images-1.medium.com/max/2400/1*FDNeKIUeUnf0XdqHmi7nsw.png", title:"Material ui", description:"We use material ui for our look and feel"}
-  ]
-
-  var webItems = [
-    {imgPath: "https://th.bing.com/th/id/OIP.MH2BhO8NKkBEcL7od69hUQHaHa?pid=ImgDet&w=600&h=600&rs=1", title:"Node JS", description:"We use Node js for our backend"},
-    {imgPath: "https://th.bing.com/th/id/OIP.CeWz1L1Ekvsg4XnopAtanAHaD4?pid=ImgDet&rs=1", title:"Typescript", description:"We use typescript as a scripting language"},
-    {imgPath: "https://th.bing.com/th/id/OIP.Mtw0eaSwwbB7D9-fFPVvcgHaDr?pid=ImgDet&rs=1", title:"Angular", description:"We use angular for our frontend"},
-  ]
-
-  var ananlyticsItems = [
-    {imgPath: "https://th.bing.com/th/id/R.b6eba8a871ac74329c23476c34956333?rik=HvzKZY4W2ZYtbw&pid=ImgRaw&r=0", title:"MySQL", description:"We use MySQL to store and query the data"},
-    {imgPath: "https://wallpapercave.com/wp/wp8583820.jpg", title:"Python", description:"We use python to shape and anylyze data"},
-    {imgPath: "https://th.bing.com/th/id/OIP.uPQgsdWJ0ccnA_j_tZjc9gHaIl?pid=ImgDet&rs=1", title:"Jupyter Notebooks", description:"We use Jupyter notebooks to visualize the data"},
+  var cardArray = [
+    {
+      title: "Apps",
+      imgPath: "https://cdn.dribbble.com/userupload/3105308/file/original-e451cc62b520510fe5e166f66791e7fb.png?compress=1&resize=840x630&vertical=to",
+      description: "We make mobile apps",
+      appItems: [
+        { imgPath: "https://img.stackshare.io/service/11331/asp.net-core.png", title: ".Net C#", description: "We use C# for our backend" },
+        { imgPath: "https://th.bing.com/th/id/OIP.HLxLvGJV-6r8ZR8AK4F9KAHaEO?pid=ImgDet&rs=1", title: "React", description: "We use react for our frontend" },
+        { imgPath: "https://cdn-images-1.medium.com/max/2400/1*FDNeKIUeUnf0XdqHmi7nsw.png", title: "Material ui", description: "We use material ui for our look and feel" }
+      ]
+    },
+    {
+      title: "Websites",
+      imgPath: "https://cdn.dribbble.com/users/15692/screenshots/1361195/attachments/194237/Flat-Dashboard-UI-real-pix.jpg?compress=1&resize=768x576&vertical=top",
+      description: "We make mobile apps",
+      appItems: [
+        { imgPath: "https://th.bing.com/th/id/OIP.MH2BhO8NKkBEcL7od69hUQHaHa?pid=ImgDet&w=600&h=600&rs=1", title: "Node JS", description: "We use Node js for our backend" },
+        { imgPath: "https://th.bing.com/th/id/OIP.CeWz1L1Ekvsg4XnopAtanAHaD4?pid=ImgDet&rs=1", title: "Typescript", description: "We use typescript as a scripting language" },
+        { imgPath: "https://th.bing.com/th/id/OIP.Mtw0eaSwwbB7D9-fFPVvcgHaDr?pid=ImgDet&rs=1", title: "Angular", description: "We use angular for our frontend" }
+      ]
+    },
+    {
+      title: "Analytics",
+      imgPath: "https://th.bing.com/th/id/OIP.1hXL7NbhaoBHaUXd1kXQtAHaHT?pid=ImgDet&rs=1",
+      description: "We make mobile apps",
+      appItems: [
+        { imgPath: "https://th.bing.com/th/id/R.b6eba8a871ac74329c23476c34956333?rik=HvzKZY4W2ZYtbw&pid=ImgRaw&r=0", title: "MySQL", description: "We use MySQL to store & query data" },
+        { imgPath: "https://wallpapercave.com/wp/wp8583820.jpg", title: "Python", description: "We use python to shape and anylyze data" },
+        { imgPath: "https://th.bing.com/th/id/OIP.uPQgsdWJ0ccnA_j_tZjc9gHaIl?pid=ImgDet&rs=1", title: "Jupyter Notebooks", description: "We use Jupyter to visualize the data" }
+      ]
+    }
   ]
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12} md={4} sm={12}>
-        <ServicesCard
-          imgUrl="https://cdn.dribbble.com/userupload/3105308/file/original-e451cc62b520510fe5e166f66791e7fb.png?compress=1&resize=840x630&vertical=top"
-          title="Apps"
-          description="We make mobile apss"
-          onClick={() => alert("Hello")}
-        />
-        <CustomAccordion items={appItems}/>
-      </Grid>
-      <Grid item xs={12} md={4} sm={12}>
-        <ServicesCard
-          imgUrl="https://cdn.dribbble.com/users/15692/screenshots/1361195/attachments/194237/Flat-Dashboard-UI-real-pix.jpg?compress=1&resize=768x576&vertical=top"
-          title="Websites"
-          description="We also make websites"
-        />
-          <CustomAccordion items={webItems}/>
-      </Grid>
-      <Grid item xs={12} md={4} sm={12}>
-        <ServicesCard
-          imgUrl="https://th.bing.com/th/id/OIP.1hXL7NbhaoBHaUXd1kXQtAHaHT?pid=ImgDet&rs=1"
-          title="Data Anylytics"
-          description="We analyze data"
-        />
-        <CustomAccordion items={ ananlyticsItems}/>
-      </Grid>
+      {
+        cardArray.map((appCard => (
+          <Grid item xs={12} md={4} sm={12}>
+            <>
+              <ServicesCard
+                imgUrl={appCard.imgPath}
+                title={appCard.title}
+                description={appCard.description}
+              />
+              <CustomAccordion items={appCard.appItems} />
+            </>
+          </Grid>
+        )))
+      }
     </Grid>
   )
 }
